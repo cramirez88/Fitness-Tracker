d81551ce73abe7d439bbd86aedbbacf2afac3016
Christian Ramirez <christian.r0602@gmail.com>
2020-11-02 20:50:11 -0600

    routes,models

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require('mongoose');
 
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 const app = express();
 
@@ -24,6 +24,6 @@ mongoose.connect("mongodb://localhost/workout", {
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
-app.removeListener(PORT, () => {
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
